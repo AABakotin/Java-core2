47d8bfe1f565ea21e00beb6e6e00a75d359fd68a
AABakotin <megabart57@gmail.com>
2021-07-29 13:40:14 +0300

    Multithreading

--- src/main/java/Multithreading/Multithreading.java
+++ src/main/java/Multithreading/Multithreading.java
@@ -7,7 +7,6 @@ public class Multithreading {
     static final int SIZE = 10000000;
     static final int HALF = SIZE / 2;
 
-
     public static void main(String[] args) {
 
         firstExample();
